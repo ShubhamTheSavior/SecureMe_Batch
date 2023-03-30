6844cb2918fd7dc9ffa7acb7ce6516e3db1a491d
ShubhamTheSavior <shubham.gadekar990@gmail.com>
2023-03-30 23:27:18 +0530

    spring batch push

---
+++ src/main/java/com/secureme/batch/SecureMeBatchApplication.java
@@ -0,0 +1,13 @@
+package com.secureme.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureMeBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureMeBatchApplication.class, args);
+	}
+
+}
